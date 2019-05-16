7469499302324a19116d07dcf1d18dc9d44f6130
Emil Madsen <emil@magenta.dk>
2019-05-16 11:26:07 +0200

    Enable changing username and groupname

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Example container for how to update UID and GID at container creation-time.
 docker build -t magentadk/docker_user_mapping .
 docker run --rm -it -e PUID=1111 -e PGID=1200 magentadk/docker_user_mapping /bin/bash
 ```
+
+## Configuration:
+The example exposes 4 environment variables:
+
+* `PUID`: Process User ID: The ID of the default user after the entry-point has run.
+* `PGID`: Process Group ID: The ID of the default group after the entry-point has run.
+* `USER`: Username: The name of the default user after the entry-point has run.
+* `GROUP`: Groupname: The name of the default group after the entry-point has run.
--- user-mapping.sh
+++ user-mapping.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 
-# We need the user to exist
-# NOTE: The user(name) cannot be changed at runtime.
-if [ -z "${USER}" ]; then
-  echo "USER variable not set, invalid image!";
-  exit 1
-fi
-
-# We need the group to exist
-# NOTE: The group(name) cannot be changed at runtime.
-if [ -z "${GROUP}" ]; then
-  echo "GROUP variable not set, invalid image!";
-  exit 1
-fi
-
-# Get active user id (either default or overridden one)
+# Get active ids (either default or overridden ones)
 USER_ID=${PUID:=$DEFAULT_PUID}
 USER_GID=${PGID:=$DEFAULT_PGID}
+# Get active names
+USER_NAME=${USER:=$DEFAULT_USER}
+GROUP_NAME=${GROUP:=$DEFAULT_GROUP}
 
 # Update user id and group id
 # NOTE: Also changes permissions of home-folder
-usermod -u ${USER_ID} ${USER}
-groupmod -g ${USER_GID} ${GROUP}
+usermod -u ${USER_ID} ${DEFAULT_USER}
+groupmod -g ${USER_GID} ${DEFAULT_GROUP}
 
 # Change ownership of ALL files to new uid/gid
-find / -xdev -user ${DEFAULT_PUID} -exec chown -h ${USER} {} \;
-find / -xdev -group ${DEFAULT_PGID} -exec chgrp -h ${GROUP} {} \;
+find / -xdev -user ${DEFAULT_PUID} -exec chown -h ${DEFAULT_USER} {} \;
+find / -xdev -group ${DEFAULT_PGID} -exec chgrp -h ${DEFAULT_GROUP} {} \;
+
+# Update username
+usermod --login ${USER_NAME} --move-home --home /home/${USER_NAME} ${DEFAULT_USER}
+groupmod --new-name ${GROUP_NAME} ${DEFAULT_GROUP}
 
 # Drop to user privilegies
 exec su - "${USER}"
